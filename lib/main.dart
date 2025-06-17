4072876c13a2c3c4e70edb0558783f181f04cfa2
bidhan-skybase <bidhan@skybase.com.np>
2025-06-17 20:40:15 +0545

    feat: Add .env configuration

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:movies_app/utils/routes.dart';
 import 'package:movies_app/views/splash_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main(){
+
+void main()async{
+  await dotenv.load(fileName: ".env");
   runApp(Flicksy());
 }
 
@@ -13,6 +16,9 @@ class Flicksy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Manrope'
+      ),
       initialRoute: SplashPage.routeName,
       getPages: getPages,
     );
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomePage extends StatelessWidget {
-  static const routeName="/home-page";
+  static const routeName = "/home-page";
+
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(child: Column(children: [Text("Home Page")])),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                var token = dotenv.env['AUTHORIZATION_TOKEN'];
+                print(token.toString());
+              },
+              child: Text("data"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
