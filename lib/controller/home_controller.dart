497bd6588bf7e442b1bfbdcfe2f03827ad6cc5d7
bidhan-skybase <bidhan@skybase.com.np>
2025-06-18 15:32:01 +0545

    feat: Add fetch movies api

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,6 +1,14 @@
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:movies_app/utils/constants/api.dart';
 
-class HomeController extends GetxController{
-
-
+class HomeController extends GetxController {
+  Future<void> fetchMovies() async {
+    var url = Uri.parse(API.fetchMovies);
+    var response = await http.get(
+      url,
+      headers: {"Authorization": "Bearer ${API.accessToken}"},
+    );
+    print(response.body);
+  }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
+import 'package:movies_app/controller/home_controller.dart';
 
 class HomePage extends StatelessWidget {
   static const routeName = "/home-page";
 
-  const HomePage({super.key});
+  final c = Get.find<HomeController>();
+
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +19,7 @@ class HomePage extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () {
-                var token = dotenv.env['AUTHORIZATION_TOKEN'];
-                print(token.toString());
+                c.fetchMovies();
               },
               child: Text("data"),
             ),
